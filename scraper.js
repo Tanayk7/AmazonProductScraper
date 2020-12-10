e944da9a6878815233f19ba0594fbf684f1217cd
MSI\Tanay <tanaykulkarni7@gmail.com>
2020-12-11 01:23:01 +0530

    moved levenshtein distance code to scraper function

--- scraper.js
+++ scraper.js
@@ -13,12 +13,14 @@ const levenshtein = require('js-levenshtein');
 //*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[14]/div/span/div/div/div[2]/h2/a/span
 //*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[15]/div/span/div/div/div[2]/h2/a/span
 
-async function scrapePrices(url,searchString,count){
+async function scrapeAmazonPrices(searchString,count){
+    let url = 'https://www.amazon.in';
     let product_name_xpath_template = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[##]/div/span/div/div/div[2]/h2/a/span';
     let product_price_xpath_template = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[##]/div/span/div/div/div[4]/div/div/div/a/span[1]/span[2]/span[2]'
     let num_results = count;
     let query = searchString;
     let products = [];
+    let output = { products: [], closestToQuery: {} };
 
     console.log("Launching browser...")
     const browser = await puppeteer.launch();
@@ -40,6 +42,7 @@ async function scrapePrices(url,searchString,count){
     await page.waitForXPath("/html/body/div[1]/div[2]/div[1]/div[2]");
     
     console.log("Populating output list...");
+    // Get prices and names from Amazon
     for(let i=0;i<num_results;i++){
         let product_name_xpath = product_name_xpath_template.replace("##",i);
         const [product_name] = await page.$x(product_name_xpath);
@@ -63,28 +66,7 @@ async function scrapePrices(url,searchString,count){
             products.push(product_info);
         }
     }
-    console.log(`${products.length} products found`);
-    console.log("Finished populating output \n");
-    return products;
-}
-
-module.exports = scrapePrices;
-
-(async () => {
-    let query = "solimo fruit and nut muesli";
-    let max_products = 200;
-
-    console.log("Search query: ",query);
-    console.log("Max products: ",max_products);
-
-    let products = await scrapePrices('https://www.amazon.in',query,max_products);
-
-    for(let product of products){
-        console.log("Product name: ",product.product_name);
-        console.log("Product price: Rs",product.product_price);
-        console.log("\n");
-    }
-
+    // Get product closest to query 
     let distances = products.map((product,index) => {
         return {
             index: index,
@@ -93,16 +75,40 @@ module.exports = scrapePrices;
     });
     let minDistance = distances[0].distance;
     let minIndex = distances[0].index;
-
     for(let i=0;i<distances.length;i++){
         if(distances[i].distance < minDistance){
             minDistance = distances[i].distance;
             minIndex = distances[i].index;
         }
     }
-
     let minDistanceProduct = products[minIndex];
-    console.log("Product closest to search query: ",minDistanceProduct);
+
+    output.products = products;
+    output.closestToQuery = minDistanceProduct;
+
+    console.log(`${products.length} products found`);
+    console.log("Finished populating output \n");
+
+    return output;
+}
+
+module.exports = scrapeAmazonPrices;
+
+(async () => {
+    let query = "solimo choco flakes";
+    let max_products = 200;
+
+    console.log("Search query: ",query);
+    console.log("Max products: ",max_products);
+
+    let output = await scrapeAmazonPrices(query,max_products);
+
+    for(let product of output.products){
+        console.log("Product name: ",product.product_name);
+        console.log("Product price: Rs",product.product_price);
+        console.log("\n");
+    }
+    console.log("Product closest to search query: ",output.closestToQuery);
     console.log("END");
 })();
 
